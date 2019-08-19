2eb1e27cfefaa393755e330ca370208ba5c044d1
premambodkar <prem.ambodkar@gmail.com>
2019-08-19 12:47:19 +0530

    project setup

---
+++ src/services/auth.config.ts
@@ -0,0 +1,10 @@
+// Change this filename to auth.config.ts
+
+export const AUTH_CONFIG = {
+  // Needed for Auth0 (capitalization: ID):
+  clientID: 'b2tBDVDVQRBX3Pz8sPIafOlcJHeHZxjn',
+  // Needed for Auth0Cordova (capitalization: Id):
+  clientId: 'b2tBDVDVQRBX3Pz8sPIafOlcJHeHZxjn',
+  domain: 'codeprank.eu.auth0.com', // e.g., you.auth0.com
+  packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
+};
